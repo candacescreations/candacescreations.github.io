158eab80d0c06ae8b3c18fc4d49d2c312ec488d1
candacescreations <163497564+candacescreations@users.noreply.github.com>
2024-03-14 22:10:28 -0700

    Add files via upload

---
+++ mobile.js
@@ -0,0 +1,4 @@
+if(/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  document.body.style.fontSize = '2.5em';
+  document.getElementById('title').style.fontSize = '1.5em';
+}
